a81a16984644448221917f7f7c2c85643cf2aee0
Denis Bain <baindenis95@gmail.com>
2024-02-29 08:29:41 -0300

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
 
   resources :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
