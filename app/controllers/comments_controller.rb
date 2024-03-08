ba95e11e25e3f6392df119847aa4f00297a6f515
Denis Bain <baindenis95@gmail.com>
2024-03-08 12:43:41 -0300

    Add authenticate before leave comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!, :only => [:create]
+  
   def create
     @article = Article.find(params[:article_id])
     @article.comments.create(comment_params)
@@ -9,6 +12,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:author, :body)
   end
 end
