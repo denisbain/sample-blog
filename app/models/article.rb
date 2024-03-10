e441afb79cb80322e352a65566ffe4d55fe3a3ea
Denis Bain <baindenis95@gmail.com>
2024-03-10 11:23:11 -0300

    add subject method for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,18 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   has_many :comments
+
+  def subject
+    title
+  end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # Создаем объект Article хитрым посоом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      #assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
 end
