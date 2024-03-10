0a961e701cfd2311b16a9aa2740772179a1e27e6
Denis Bain <baindenis95@gmail.com>
2024-03-10 10:32:05 -0300

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 require "rails_helper"
 
 describe Article, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:text) }
+  end
 
-  it { should have_many :comments }
+  describe "associations" do
+    it { should have_many :comments }
+  end
 end
