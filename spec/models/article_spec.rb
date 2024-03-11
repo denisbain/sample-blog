25356555dbfca9ce98c75d6fd459b8b4706cb913
Denis Bain <baindenis95@gmail.com>
2024-03-11 10:21:04 -0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,4 +10,24 @@ describe Article, type: :model do
   describe "associations" do
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # Создаем объект Article хитрым посоом
+      #article = create(:article, title: 'Lorem Ipsum')
+
+      #assert, проверка
+      #expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      #Создаем статью но в это раз с комментариями
+      #article = create(:article_with_comments)
+
+      #проверка
+      # expect(article.last_comment.body).to eq "comment body 3"
+    end
+  end
 end
