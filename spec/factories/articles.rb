ec490f4a6d4156728cdbbb2fb6b27aed602ce472
Denis Bain <baindenis95@gmail.com>
2024-03-11 10:20:02 -0300

    Add comment factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
     title { "Article title" }
     text { "Article text" }
+
+    # создаем фабрику с названием article_with_comments
+    # для создания статьи с несколькими комментариями
+    factory :article_with_comments do
+      #После создания article
+      after :create do |article, evaluator|
+        #Создаем список из трех комментариев
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
