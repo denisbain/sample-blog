4b39055137d88acbafb623dea25542623bbc8b7c
Denis Bain <baindenis95@gmail.com>
2024-03-13 08:39:38 -0300

    Add article creation test

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -8,4 +8,11 @@ feature "Article Creation" do
     visit new_article_path
     expect(page).to have_content I18n.t('articles.new_article')
   end
+  scenario "allows user to create new article" do
+    visit new_article_path
+    fill_in :article_title, :with => "something"
+    fill_in :article_text, :with => "something"
+    click_button 'Save article'
+    expect(page).to have_content I18n.t('articles.create_article')
+  end
 end
