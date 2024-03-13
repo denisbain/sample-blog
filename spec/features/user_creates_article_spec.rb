51c580b6bbe8d809a414031ed52f7f7b1c358f32
Denis Bain <baindenis95@gmail.com>
2024-03-13 07:31:47 -0300

    Update tests (allows user to visit article page)

---
+++ spec/features/user_creates_article_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "Article Creation" do
+  before(:each) do
+    sign_up
+  end
+  scenario "allows user to visit article page" do
+    visit new_article_path
+    expect(page).to have_content 'New article'
+  end
+end
